9da5d6446e23bb53e853a8e4d4ba2e27c064bf95
Simon <simon.tessard@gmail.com>
2024-01-26 17:44:18 +0100

    More JWT testing

--- back/src/test/java/com/openclassrooms/starterjwt/security/jwt/JwtUtilsTests.java
+++ back/src/test/java/com/openclassrooms/starterjwt/security/jwt/JwtUtilsTests.java
@@ -6,7 +6,12 @@ import org.mockito.Mock;
 import org.springframework.security.core.Authentication;
 
 import com.openclassrooms.starterjwt.security.services.UserDetailsImpl;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
 import java.lang.reflect.Field;
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -66,5 +71,44 @@ public class JwtUtilsTests {
         boolean isValid = jwtUtils.validateJwtToken(token);
 
         assertTrue(isValid);
+    }
+
+    @Test
+    public void testValidateJwtToken_InvalidSignature() {
+        String invalidSignatureToken = "tokenWithInvalidSignature";
+        assertFalse(jwtUtils.validateJwtToken(invalidSignatureToken));
+    }
+
+    @Test
+    public void testValidateJwtToken_MalformedToken() {
+        String malformedToken = "125436587894498de";
+        assertFalse(jwtUtils.validateJwtToken(malformedToken));
+    }
+
+    @Test
+    public void testValidateJwtToken_ExpiredToken() throws InterruptedException {
+        // Générer un token qui expire après 1 seconde
+        String jwtSecret = "yourSecretKey";
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + 1000);
+
+        String expiredToken = Jwts.builder()
+                .setSubject("testUser")
+                .setIssuedAt(new Date())
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+
+        // Attendre 2 secondes pour que le token expire
+        Thread.sleep(2000);
+
+        // Vérifier que le token est expiré
+        assertFalse(jwtUtils.validateJwtToken(expiredToken));
+    }
+
+    @Test
+    public void testValidateJwtToken_UnsupportedToken() {
+        String unsupportedToken = "087258a5-ddb2-487e-a38e-071698896ff9";
+        assertFalse(jwtUtils.validateJwtToken(unsupportedToken));
     }
 }
